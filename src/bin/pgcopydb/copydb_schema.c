edfe8f5d13c060d48b0776d64fa488190d959df0
Alexander Alexandrowitz <alexandrowitz@modell-aachen.de>
2024-09-13 11:32:35 +0200

    fixed logs

--- src/bin/pgcopydb/copydb_schema.c
+++ src/bin/pgcopydb/copydb_schema.c
@@ -971,13 +971,13 @@ copydb_fetch_filtered_oids(CopyDataSpec *specs, PGSQL *pgsql)
 		if (!schema_list_ext_schemas(pgsql, filtersDB))
 		{
 			/* errors have already been logged */
-			log_debug("copydb_schema: 1")
+			log_debug("copydb_schema: 1");
 			(void) semaphore_unlock(&(filtersDB->sema));
 			return false;
 		}
 	}
 
-	log_debug("copydb_schema: 2")
+	log_debug("copydb_schema: 2");
 	if (specs->skipCollations &&
 		!filtersDB->sections[DATA_SECTION_COLLATIONS].fetched)
 	{
@@ -1014,7 +1014,7 @@ copydb_fetch_filtered_oids(CopyDataSpec *specs, PGSQL *pgsql)
 				 (long long) count.colls);
 	}
 
-	log_debug("copydb_schema: 3")
+	log_debug("copydb_schema: 3");
 	/*
 	 * Take the complement of the filtering, to list the OIDs of objects that
 	 * we do not process.
@@ -1066,7 +1066,7 @@ copydb_fetch_filtered_oids(CopyDataSpec *specs, PGSQL *pgsql)
 		return true;
 	}
 
-	log_debug("copydb_schema: 4")
+	log_debug("copydb_schema: 4");
 	/*
 	 * Now fetch the OIDs of tables, indexes, and sequences that we filter out.
 	 */
@@ -1098,7 +1098,7 @@ copydb_fetch_filtered_oids(CopyDataSpec *specs, PGSQL *pgsql)
 			return false;
 		}
 	}
-	log_debug("copydb_schema: 5")
+	log_debug("copydb_schema: 5");
 	if ((specs->section == DATA_SECTION_ALL ||
 		 specs->section == DATA_SECTION_INDEXES ||
 		 specs->section == DATA_SECTION_CONSTRAINTS) &&
@@ -1142,7 +1142,7 @@ copydb_fetch_filtered_oids(CopyDataSpec *specs, PGSQL *pgsql)
 			return false;
 		}
 	}
-	log_debug("copydb_schema: 6")
+	log_debug("copydb_schema: 6");
 	if ((specs->section == DATA_SECTION_ALL ||
 		 specs->section == DATA_SECTION_SET_SEQUENCES) &&
 		!filtersDB->sections[DATA_SECTION_SET_SEQUENCES].fetched)
@@ -1171,7 +1171,7 @@ copydb_fetch_filtered_oids(CopyDataSpec *specs, PGSQL *pgsql)
 			return false;
 		}
 	}
-	log_debug("copydb_schema: 7")
+	log_debug("copydb_schema: 7");
 
 	if (!filtersDB->sections[DATA_SECTION_DEPENDS].fetched)
 	{
@@ -1199,7 +1199,7 @@ copydb_fetch_filtered_oids(CopyDataSpec *specs, PGSQL *pgsql)
 			return false;
 		}
 	}
-	log_debug("copydb_schema: 8")
+	log_debug("copydb_schema: 8");
 	/* re-install the actual filter type */
 	filters->type = type;
 
